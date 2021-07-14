4746b411d0557cbdfd97ef27e453bca9a9551bd1
seol <seolnavy@naver.com>
2021-07-14 22:49:00 +0900

    20210714

--- thymeleaf-basic/src/main/java/seol/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/seol/thymeleaf/basic/BasicController.java
@@ -1,5 +1,12 @@
 package seol.thymeleaf.basic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpSession;
+import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +25,50 @@ public class BasicController {
 	public String textUnescaped(Model model) {
 		model.addAttribute("data", "Hello <b>Spring!</b>");
 		return "basic/text-unescaped";
+	}
+
+	@GetMapping("/variable")
+	public String variable(Model model) {
+		User userA = new User("userA", 10);
+		User userB = new User("userA", 10);
+
+		List<User> list = new ArrayList<>();
+		list.add(userA);
+		list.add(userB);
+		Map<String, User> map = new HashMap<>();
+		map.put("userA", userA);
+		map.put("userB", userB);
+
+		model.addAttribute("user", userA);
+		model.addAttribute("users", list);
+		model.addAttribute("userMap", map);
+		return "basic/variable";
+	}
+
+	@Data
+	static class User {
+
+		private String username;
+		private int age;
+
+		public User(String username, int age) {
+			this.username = username;
+			this.age = age;
+		}
+	}
+
+	@GetMapping("/basic-objects")
+	public String basicObjects(HttpSession session) {
+		session.setAttribute("sessionData", "Hello Session");
+		return "basic/basic-objects";
+	}
+
+	@Component("helloBean")
+	static class HelloBean {
+
+		public String hello(String data) {
+			return "Hello " + data;
+		}
 	}
 
 }
