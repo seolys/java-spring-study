54cdd93b00de22da71113d5f3f8ae1b911c9461b
설연수 <seolys@marketboro.com>
2021-09-11 21:43:55 +0900

    @ExceptionHandler, @RestControllerAdvice

--- exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -1,22 +1,39 @@
 package hello.exception.api;
 
-import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
-public class ApiExceptionController {
+public class ApiExceptionV2Controller {
 
-	@GetMapping("/api/members/{id}")
+//	@ResponseStatus(HttpStatus.BAD_REQUEST)
+//	@ExceptionHandler(IllegalArgumentException.class)
+//	public ErrorResult illegalExHandler(IllegalArgumentException e) {
+//		log.error("[exceptionHandler] ex", e);
+//		return new ErrorResult("BAD", e.getMessage());
+//	}
+//
+//	@ExceptionHandler
+//	public ResponseEntity<ErrorResult> userExHandler(UserException e) {
+//		log.error("[exceptionHandler] ex", e);
+//		ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+//		return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+//	}
+//
+//	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//	@ExceptionHandler
+//	public ErrorResult exHandler(Exception e) {
+//		log.error("[exceptionHandler] ex", e);
+//		return new ErrorResult("EX", "내부 오류");
+//	}
+
+	@GetMapping("/api2/members/{id}")
 	public MemberDto getMemberId(@PathVariable("id") String id) {
 		if (id.equals("ex")) {
 			throw new RuntimeException("잘못된 사용자");
@@ -28,21 +45,6 @@ public class ApiExceptionController {
 			throw new UserException("사용자 오류");
 		}
 		return new MemberDto(id, "hello " + id);
-	}
-
-	@GetMapping("/api/response-status-ex1")
-	public String responseStatusEx1() {
-		throw new BadRequestException();
-	}
-
-	@GetMapping("/api/response-status-ex2")
-	public String responseStatusEx2() {
-		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
-	}
-
-	@GetMapping("/api/default-handler-ex")
-	public String defaultException(@RequestParam Integer data) {
-		return "OK";
 	}
 
 	@Data
